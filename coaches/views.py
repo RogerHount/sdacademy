4da8cac0984e44ba3f883e840a3eaafa8ed13865
Sergey Loginov <sergeiloginov91@gmail.com>
2016-02-28 09:21:37 +0200

    initial for KZ10

--- coaches/views.py
+++ coaches/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from coaches.models import Coach
-
+from courses.models import Course
 
 def detail(request, id_of_coach):
     defined_coach = Coach.objects.get(id = id_of_coach)
-#   list_of_lessons = Lesson.objects.filter(course = id_of_course)
-    return render(request, 'coaches/detail.html', {'defined_coach' : defined_coach})
+    defined_course_coach = Course.objects.filter(coach = id_of_coach)
+    defined_course_assistant = Course.objects.filter(assistant = id_of_coach)
+
+    return render(request, 'coaches/detail.html', {'defined_coach' : defined_coach, 'defined_course_coach' : defined_course_coach, 'defined_course_assistant' : defined_course_assistant})
