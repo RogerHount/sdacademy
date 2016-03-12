354b054ac6728b346ba29bcd1967fc8aac4dd4bd
Roger Hount <sergey.loginov@toatech.com>
2016-03-12 10:03:00 +0200

    add.remove.edit  /

--- students/urls.py
+++ students/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
 	url(r'^$', list_view, name='list_view'),
     url(r'^(?P<id_of_student>\d+)/$', detail, name='detail'),
     url(r'^add/$', create, name='add'),
-    url(r'^edit/(?P<student_to_edit>\d+)$', edit, name='edit'),
-    url(r'^remove/(?P<student_to_remove>\d+)$', remove, name='remove'),
+    url(r'^edit/(?P<student_to_edit>\d+)/$', edit, name='edit'),
+    url(r'^remove/(?P<student_to_remove>\d+)/$', remove, name='remove'),
 )
