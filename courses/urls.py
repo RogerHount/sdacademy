ffeb0c384d43aeb1c2bc80de5429e47ced5aeb14
Roger Hount <sergey.loginov@toatech.com>
2016-03-12 12:53:28 +0200

    12.2 added /

--- courses/urls.py
+++ courses/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     url(r'^add/$', add, name='add'),
     url(r'^edit/(?P<id_of_course>\d+)/$', edit, name='edit'),
     url(r'^remove/(?P<id_of_course>\d+)/$', remove, name='remove'),
-    url(r'^(?P<id_of_course>\d+)/add-lesson$', add_lesson, name='add_lesson'),
+    url(r'^(?P<id_of_course>\d+)/add-lesson/$', add_lesson, name='add_lesson'),
 )
